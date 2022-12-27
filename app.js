740fa72cc68275b176b7574847714f3814f84851
Mrinal Ghosh <ghoshmrinal30@gmail.com>
2022-12-27 14:02:33 +0530

    encrytion added level one

---
+++ app.js
@@ -0,0 +1,107 @@
+//jshint esversion:6
+
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const express=require("express");
+const { appendFile } = require("fs");
+const mongoose=require("mongoose");
+const mongooseEncryption=require("mongoose-encryption");
+
+
+
+const app =express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{ useNewUrlParser: true});
+
+
+var userSchema=new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret="thisisthesecret";
+
+userSchema.plugin(mongooseEncryption, {secret: secret, encryptedFields: ["password"]});
+
+const user= mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res)
+{
+    res.render("home");
+})
+
+app.get("/login", function(req,res)
+{
+    res.render("login");
+})
+
+app.get("/register", function(req,res)
+{
+    res.render("register");
+})
+
+
+app.post("/register", function(req,res)
+{
+    const userName=req.body.username;
+    const password=req.body.password;
+
+    const newUser=new user({
+        email: userName,
+        password: password
+})
+
+newUser.save(function(err)
+{
+    if(err)
+    {
+        console.log("failed to store");
+    }
+    else{
+        res.render("secrets");
+    }
+})
+
+app.post("/login", function(req,res)
+{
+
+    const userName=req.body.username;
+    const password=req.body.password;
+
+    user.findOne({email: userName}, function(err, foundData)
+    {
+        if(err)
+        {
+            console.log("error in process");
+        }
+        else{
+            if(foundData)
+            {
+                if(foundData.password===password)
+                {
+                    res.render("secrets");
+                }
+                else{
+                    console.log("data doesn't match");
+                }
+            }
+        }
+    })
+
+})
+
+
+})
+
+
+app.listen(3000, function()
+{
+    console.log("server is listening on port 3000");
+})
