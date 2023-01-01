365c39605e6b458e0f7253afd3b50a36b4a096b8
Mrinal Ghosh <ghoshmrinal30@gmail.com>
2023-01-01 09:59:05 +0530

    Submission of secret and OAuth2O Google Add

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const mongoose=require("mongoose");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
-const { use } = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 
 const app =express();
@@ -36,10 +38,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",{ useNewUrlParser: true});
 
 var userSchema=new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 //console.log(process.env.SECRET);
 //userSchema.plugin(mongooseEncryption, {secret: process.env.SECRET, encryptedFields: ["password"]});
@@ -48,14 +53,55 @@ const user= mongoose.model("User", userSchema);
 
 passport.use(user.createStrategy());
 
-passport.serializeUser(user.serializeUser());
-passport.deserializeUser(user.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log("access token: "+accessToken);
+    user.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req, res)
 {
     res.render("home");
 })
+
+
+//try to authenticate google side
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+  );
+
+
+//once authenticated we will authenticate our side where this call back route comes into picture
+// then it returns the access token or it calls the call back function
+  app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req,res)
 {
@@ -67,15 +113,57 @@ app.get("/register", function(req,res)
     res.render("register");
 })
 
-app.get("/secret", function(req,res)
+app.get("/submit", function(req,res)
 {
-    if(req.isAuthenticated())
+    if(req.isAuthenticated)
     {
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req,res)
+{
+    const submittedData=req.body.secret;
+    console.log(req.user);
+
+    user.findById(req.user.id, function(err,foundData)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            foundData.secret=submittedData;
+            foundData.save(function(err)
+            {
+                if(err)
+                {
+                    console.log("data unable to update"+err);
+                }
+                else{
+                    res.redirect("/secrets");
+                }
+            })
+        }
+    })
+
+})
+
+app.get("/secrets", function(req,res)
+{
+    user.find({secret: {$ne: null}}, function(err, foundData)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            res.render("secrets",{foundUser: foundData});
+        }
+    })
 })
 
 app.post("/register", function(req,res)
@@ -122,7 +210,7 @@ app.post("/login", function(req,res)
         else{
             passport.authenticate("local")(req,res, function()
             {
-                res.redirect("/secret");
+                res.redirect("/secrets");
             })
         }
     })
